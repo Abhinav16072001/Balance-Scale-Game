2bc8ab6cbe38cab9d6542b2c132e07a407438237
Abhinav <66725911+Abhinav16072001@users.noreply.github.com>
2024-01-04 01:31:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import getpass
+
+
 class Player:
     def __init__(self, name):
         self.points = 0
         self.name = name
         self.number = None
+
 
 class Game:
     def __init__(self, n, limit):
@@ -14,50 +18,63 @@ class Game:
 
     def start(self):
         for player in range(self.num_of_players):
-            name = input(f"Player {player}:")
+            name = input(f"Player {player + 1}:")
             self.players.append(Player(name))
 
     def calulate_score(self, numbers):
         score = (sum(numbers)/len(numbers))*0.8
         return score
-    
-        
+
     def start_rounds(self):
         numbers = []
-        min_score_player = self.players[0]
+        min_score_player = []
         while len(self.players) != 1:
 
+            count = 0
             for player in self.players:
-                num = int(input(f"Player {player.name} choose a number (0 - 100): "))
-                player.number = num
-                numbers.append(num)
+                while True:
+                    try:
+                        num = int(getpass.getpass(
+                            f"Player {player.name} choose a number (0 - 100): "))
+                        player.number = num
+                        numbers.append(num)
+                        break
+                    except:
+                        print("Invalid input. Please enter a valid number.")
 
+            score = self.calulate_score(numbers)
 
-            score =  self.calulate_score(numbers)
+            for player in self.players:
+                print(f"{player.name}: {player.number}", end=' ')
+
+            numbers.clear()
             print("Score: ", score)
             min_diff = 100
             for player in self.players:
-                diff  = abs(player.number - score)
+                diff = abs(player.number - score)
                 if min_diff > diff:
-                    min_score_player = player
+                    min_score_player = [player]
                     min_diff = diff
-            
+                elif min_diff == diff:
+                    min_score_player.append(player)
+
             for player in self.players:
                 player.number = None
-                if player is min_score_player:
+                if player in min_score_player:
                     continue
                 player.points -= 1
                 if player.points <= self.limit:
                     self.players.remove(player)
                     print(f'Player {player.name} has been eliminated')
-            
+
+            min_score_player.clear()
+
             print([f'{i.name}: {i.points}' for i in self.players])
 
         print(f'Player {self.players[0].name} won')
         return self.players[0]
 
 
-    
-game = Game(3, -2)
+game = Game(3, -10)
 game.start()
 game.start_rounds()
